36c7de2cd2b3282b43dc37af4dc9ec4f41cb3404
Mike Gonzalez <Mike.Gon1217@gmail.com>
2022-11-03 15:12:24 -0400

    adjusting logic

--- logic/script.js
+++ logic/script.js
@@ -7,7 +7,13 @@ let weather = {
         "&units=imperial&appid=" +
         this.apiKey
     )
-      .then((response) => response.json())
+      .then((response) => {
+        if (!response.ok) {
+          alert("No weather found.");
+          throw new Error("No weather found.");
+        }
+        return response.json();
+      })
       .then((data) => this.displayWeather(data));
   },
   displayWeather: function (data) {
@@ -15,15 +21,18 @@ let weather = {
     const { icon, description } = data.weather[0];
     const { temp, humidity } = data.main;
     const { speed } = data.wind;
-    console.log(name, icon, description, temp, humidity, speed);
     document.querySelector(".city").innerText = "Weather in " + name;
     document.querySelector(".icon").src =
-      "https://openweathermap.org/img/wn/" + icon + "@2x.png";
+      "https://openweathermap.org/img/wn/" + icon + ".png";
     document.querySelector(".description").innerText = description;
     document.querySelector(".temp").innerText = temp + "°F";
     document.querySelector(".humidity").innerText =
       "Humidity: " + humidity + "%";
-    document.querySelector(".wind").innerText = "Wind speed: " + speed + " mph";
+    document.querySelector(".wind").innerText =
+      "Wind speed: " + speed + " mp/h";
+    document.querySelector(".weather").classList.remove("loading");
+    document.body.style.backgroundImage =
+      "url('https://source.unsplash.com/1600x900/?" + name + "')";
   },
   search: function () {
     this.fetchWeather(document.querySelector(".search-bar").value);
@@ -33,3 +42,13 @@ let weather = {
 document.querySelector(".search button").addEventListener("click", function () {
   weather.search();
 });
+
+document
+  .querySelector(".search-bar")
+  .addEventListener("keyup", function (event) {
+    if (event.key == "Enter") {
+      weather.search();
+    }
+  });
+
+weather.fetchWeather("Atlanta");
